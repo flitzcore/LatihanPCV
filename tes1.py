cf5317c575fcf53cee6c8f6dc7663761e3056e29
LockRR <hemistergt@gmail.com>
2022-10-08 22:10:19 +0700

    up: result v1 :rocket:

--- tes1.py
+++ tes1.py
@@ -1,24 +1,30 @@
 import cv2 as cv
 import numpy as np
-import tkinter as tk
-from tk import *
-sensitivity =30
-low_green=(60 - sensitivity, 100, 50)  
-high_green= (60 + sensitivity, 255, 255)
+
+
+def on_trackbar(val):
+    return
+
+
 init_red = 0
 init_green = 0
 init_blue = 0
 
-root=tk.Tk()
-w = tk.Scale(root, from_=0, to=42)
-w.pack()
 vid = cv.VideoCapture(0)
+cv.namedWindow('result')
+cv.createTrackbar('g', 'result', 0, 255, on_trackbar)
+
+
 while(True):
     _, videoCam= vid.read()
 
     f_height,f_width,_=videoCam.shape
     hsv = cv.cvtColor(videoCam, cv.COLOR_BGR2HSV)
 
+    sensitivity=cv.getTrackbarPos('g','result')
+
+    low_green=(60 - sensitivity, 100, 50)  
+    high_green= (60 + sensitivity, 255, 255)
     mask1= cv.inRange(hsv, low_green , high_green )
 
     mask1=cv.bitwise_not(mask1)
@@ -36,7 +42,7 @@ while(True):
     blank_image[:,0:f_width] =res # ingat dalam BGR
     blank_image[:,f_width:f_width*2] = mask1
     
-    cv.imshow('',blank_image)
+    cv.imshow('result',blank_image)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
     
