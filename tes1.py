6b91c107c42d4706f95caf372e26b6369e4fdcd2
LockRR <hemistergt@gmail.com>
2022-10-10 23:02:55 +0700

    before ex

--- tes1.py
+++ tes1.py
@@ -5,33 +5,39 @@ import numpy as np
 def on_trackbar(val):
     return
 
-
-init_red = 0
-init_green = 0
-init_blue = 0
-
+bg=cv.imread('image.jpg')
 vid = cv.VideoCapture(0)
 cv.namedWindow('result')
 cv.createTrackbar('g', 'result', 0, 255, on_trackbar)
-
+#cv.createTrackbar('b', 'result', 0, 255, on_trackbar)
 
 while(True):
     _, videoCam= vid.read()
 
     f_height,f_width,_=videoCam.shape
+    bg = cv.resize(bg, (f_width,f_height), interpolation = cv.INTER_AREA)
     hsv = cv.cvtColor(videoCam, cv.COLOR_BGR2HSV)
 
-    sensitivity=cv.getTrackbarPos('g','result')
-
-    low_green=(60 - sensitivity, 100, 50)  
-    high_green= (60 + sensitivity, 255, 255)
+    g_sensitivity=cv.getTrackbarPos('g','result')
+   # b_sensitivity=cv.getTrackbarPos('b','result')
+    
+    low_green=(105 - g_sensitivity, 100, 50)  
+    high_green= (105 + g_sensitivity, 255, 255)
+    #low_blue = (105-b_sensitivity,100,50)
+    #high_blue = (105+b_sensitivity,255,255)
     mask1= cv.inRange(hsv, low_green , high_green )
-
+    #mask2= cv.inRange(hsv, low_blue , high_blue)
     mask1=cv.bitwise_not(mask1)
-    res = cv.bitwise_and(videoCam, videoCam, mask = mask1)
-    
+    #mask2=cv.bitwise_not(mask2)
+    masked = cv.bitwise_and(videoCam, videoCam, mask = mask1)
+   # masked = cv.bitwise_and(videoCam, videoCam, mask = mask2)
+    res = np.where(masked == 0, bg, masked)
+
     mask1=mask1[:,:,np.newaxis]
-    
+   # mask2=mask2[:,:,np.newaxis]
+   # mask_total=cv.bitwise_and(mask1, mask1, mask = mask2)
+   # mask_total=mask_total[:,:,np.newaxis]
+   # print(mask_total.shape)
 
     blank_image = np.zeros((f_height,f_width*2,3), np.uint8)
 
@@ -39,7 +45,7 @@ while(True):
     ## isi masking
     
 
-    blank_image[:,0:f_width] =res # ingat dalam BGR
+    blank_image[:,0:f_width] =res# ingat dalam BGR
     blank_image[:,f_width:f_width*2] = mask1
     
     cv.imshow('result',blank_image)
--- tes3.py
+++ tes3.py
@@ -1,66 +1,49 @@
+import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button
 
 
-# The parametrized function to be plotted
-def f(t, amplitude, frequency):
-    return amplitude * np.sin(2 * np.pi * frequency * t)
-
-t = np.linspace(0, 1, 1000)
-
-# Define initial parameters
-init_amplitude = 5
-init_frequency = 3
-
-# Create the figure and the line that we will manipulate
-fig, ax = plt.subplots()
-line, = ax.plot(t, f(t, init_amplitude, init_frequency), lw=2)
-ax.set_xlabel('Time [s]')
-
-# adjust the main plot to make room for the sliders
-fig.subplots_adjust(left=0.25, bottom=0.25)
-
-# Make a horizontal slider to control the frequency.
-axfreq = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-freq_slider = Slider(
-    ax=axfreq,
-    label='Frequency [Hz]',
-    valmin=0.1,
-    valmax=30,
-    valinit=init_frequency,
-)
-
-# Make a vertically oriented slider to control the amplitude
-axamp = fig.add_axes([0.5, 0.25, 0.0225, 0.63])
-amp_slider = Slider(
-    ax=axamp,
-    label="Amplitude",
-    valmin=0,
-    valmax=10,
-    valinit=init_amplitude,
-    orientation="vertical"
-)
+def on_trackbar(val):
+    return
 
 
-# The function to be called anytime a slider's value changes
-def update(val):
-    line.set_ydata(f(t, amp_slider.val, freq_slider.val))
-    fig.canvas.draw_idle()
+bg=cv.imread('image.jpg')
+vid = cv.VideoCapture(0)
+cv.namedWindow('result')
+cv.createTrackbar('g', 'result', 0, 255, on_trackbar)
 
 
-# register the update function with each slider
-freq_slider.on_changed(update)
-amp_slider.on_changed(update)
+while(True):
+    _, videoCam= vid.read()
 
-# Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
+    f_height,f_width,_=videoCam.shape
+    bg = cv.resize(bg, (f_width,f_height))
+    hsv = cv.cvtColor(videoCam, cv.COLOR_BGR2HSV)
+
+    sensitivity=cv.getTrackbarPos('g','result')
+
+    low_green=(60 - sensitivity, 100, 50)  
+    high_green= (60 + sensitivity, 255, 255)
+    mask1= cv.inRange(hsv, low_green , high_green )
+
+    mask1=cv.bitwise_not(mask1)
+    masked = cv.bitwise_and(videoCam, videoCam, mask = mask1)
+    res = np.where(masked == 0, bg, masked)
+    mask1=mask1[:,:,np.newaxis]
+    
+
+    blank_image = np.zeros((f_height,f_width*2,3), np.uint8)
 
 
-def reset(event):
-    freq_slider.reset()
-    amp_slider.reset()
-button.on_clicked(reset)
+    ## isi masking
+    
 
-plt.show()
+    blank_image[:,0:f_width] =res # ingat dalam BGR
+    blank_image[:,f_width:f_width*2] = mask1
+    
+    cv.imshow('result',blank_image)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+
+vid.release()
+cv.destroyAllWindows()
